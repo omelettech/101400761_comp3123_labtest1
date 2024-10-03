6c34d39816bb8d5c186b393dc7c1e29205278997
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-10-03 13:38:34 -0400

    Completed Question1

---
+++ question1/question-1.js
@@ -0,0 +1,22 @@
+async function lowerCaseWords(arr){
+    let new_arr=[]
+    return new Promise((resolve,reject)=>{
+        if(!Array.isArray(arr)){
+            reject("Input should be an array")
+        }
+        else{
+            for(let i=0;i<arr.length;i++){
+                if(typeof arr[i] === 'string'){
+                    new_arr.push(arr[i].toLowerCase())
+                }
+            }
+            resolve(new_arr)
+        }
+    })
+
+
+}
+
+lowerCaseWords(["Pizza",1,2,"PIZZAAA"])
+    .then(result => console.log(result))  // Output: ['hello', 'world', 'javascript']
+    .catch(error => console.error(error));
